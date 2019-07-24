654a73b48c51bc8bc7729ca3d0575b1ad737b6e0
berktepebag <berktepebag@gmail.com>
2019-07-24 12:01:44 +0200

    Update Follow Interactive Marker
    TeleopCallBack checks if received pose is empty, if not fills the geometry_msgs::Pose and sets has_new_msg boolean as true.
    
    In main loop while(ros:ok())
    
    If new message arrived sending IK solver a request, and waiting for the response. When response received as joint positions fills the joint_group_positions vector to be send as command.
    
    When no new message arrives (interactive marker does not move) last position of the interactive marker will be converted to joint space and sent as command.

--- src/follow_interactive_marker.cpp
+++ src/follow_interactive_marker.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <fstream>
+#include <math.h>
 // Kinematics
 #include <moveit_msgs/GetPositionIK.h>
 
+bool has_new_msg = false;
 geometry_msgs::Pose planned_pose;
 std::vector<double> joint_group_positions = {0*M_PI/180, -M_PI_4*M_PI/180, 0*M_PI/180, -3 * M_PI_4*M_PI/180, 0*M_PI/180, M_PI_2*M_PI/180, M_PI_4*M_PI/180};
 
@@ -12,10 +14,12 @@ void frankaRobotTeleopCallback(const visualization_msgs::InteractiveMarkerUpdate
 
 	ROS_INFO("Received message");
 
-	planned_pose = msg->poses[0].pose;	
+	has_new_msg = false;
+	if(!msg->poses.empty()){
+		planned_pose = msg->poses[0].pose;	
+		has_new_msg = true;
+	}
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -54,52 +58,72 @@ int main(int argc, char **argv)
 	franka_robot.setJointStartPosition();
 	joint_state_msg.position = franka_robot.getJointGoalPositions();	
 
-	//Set joint velocities to zero in order to prevent unexpected movements at the beginning.
-	franka_robot.setJointVelocitiesZero();	
+	bool printing = false;
 
-	//nh.getParam("panda_joi")	
-
-	srand(time(NULL));
-	
-	while(ros::ok()){		
-
-		franka_robot.setJointVelocitiesOne();
-
-		/*
-		ROS_INFO("Pose x: %f", planned_pose.position.x);
-		ROS_INFO("Pose y: %f", planned_pose.position.y);
-		ROS_INFO("Pose z: %f", planned_pose.position.z);
-		*/
+	while(ros::ok()){	
 
 
-		service_request.ik_request.group_name = "panda_arm";
-		service_request.ik_request.pose_stamped.pose.position.x = planned_pose.position.x;
-		service_request.ik_request.pose_stamped.pose.position.y = planned_pose.position.y;
-		service_request.ik_request.pose_stamped.pose.position.z = planned_pose.position.z;
 
-		service_request.ik_request.pose_stamped.pose.orientation.x = planned_pose.orientation.x;
-		service_request.ik_request.pose_stamped.pose.orientation.y = planned_pose.orientation.y;
-		service_request.ik_request.pose_stamped.pose.orientation.z = planned_pose.orientation.z;
-		service_request.ik_request.pose_stamped.pose.orientation.w = planned_pose.orientation.w;
+		if (has_new_msg & printing)
+		{
+			ROS_INFO("Pose x: %f", planned_pose.position.x);
+			ROS_INFO("Pose y: %f", planned_pose.position.y);
+			ROS_INFO("Pose z: %f", planned_pose.position.z);
 
-  		//Call the service
-		service_client.call(service_request, service_response);
-		ROS_INFO_STREAM(
-			"Result: " << ((service_response.error_code.val == service_response.error_code.SUCCESS) ? "True " : "False ")
-			<< service_response.error_code.val);
-
-		
-
-
-		for (int i = 0; i < 7; ++i)
-		{		
-			ROS_INFO("Converted from Pose to Joint Space with IK-> Joint %d position: %f",i,service_response.solution.joint_state.position[i]*180/M_PI);		
-			joint_group_positions[i] = 	service_response.solution.joint_state.position[i];
+			ROS_INFO("Rot x: %f", planned_pose.orientation.x);
+			ROS_INFO("Rot y: %f", planned_pose.orientation.y);
+			ROS_INFO("Rot z: %f", planned_pose.orientation.z);
+			ROS_INFO("Rot w: %f", planned_pose.orientation.w);
 		}
 		
+		if (has_new_msg)
+		{
+			/* code */
+			service_request.ik_request.group_name = "panda_arm";
+			service_request.ik_request.pose_stamped.pose.position.x = planned_pose.position.x;
+			service_request.ik_request.pose_stamped.pose.position.y = planned_pose.position.y;
+			service_request.ik_request.pose_stamped.pose.position.z = planned_pose.position.z;
 
-		joint_state_msg.velocity = {0.02,0.02,0.02,0.02,0.02,0.02,0.02};
-		joint_state_msg.position = joint_group_positions;
+			service_request.ik_request.pose_stamped.pose.orientation.x = planned_pose.orientation.x;
+			service_request.ik_request.pose_stamped.pose.orientation.y = planned_pose.orientation.y;
+			service_request.ik_request.pose_stamped.pose.orientation.z = planned_pose.orientation.z;
+			service_request.ik_request.pose_stamped.pose.orientation.w = planned_pose.orientation.w;
+
+  		//Call the service
+			service_client.call(service_request, service_response);
+			ROS_INFO_STREAM(
+				"Result: " << ((service_response.error_code.val == service_response.error_code.SUCCESS) ? "True " : "False ")
+				<< service_response.error_code.val);
+
+
+			for (int i = 0; i < 7; ++i)
+			{		
+				ROS_INFO("Converted from Pose to Joint Space with IK-> Joint %d position: %f",i,service_response.solution.joint_state.position[i]*180/M_PI);		
+				joint_group_positions[i] = 	service_response.solution.joint_state.position[i];
+			}
+		}
+		if (!has_new_msg)
+		{
+			ROS_INFO("***************-***************");
+			for (int i = 0; i < 7; ++i)
+			{
+				ROS_INFO("Joint %d Pos: %f",i,joint_group_positions[i]);
+			}
+			joint_state_msg.velocity = {5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180};
+			joint_state_msg.position = joint_group_positions;	
+		}
+
+		//ROS_INFO("Time %f: ", ros::Time::now().toSec());
+		
+		// int choice = -1;
+		// std::cout << "Please press 0 to move to goal poisition!" << std::endl;
+		// std::cin >> choice;
+		// if (choice==0)
+		// {
+		//joint_state_msg.velocity = {5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180,5*M_PI/180};
+		//joint_state_msg.position = joint_group_positions;			
+		//}
+
 
 		teleop_cmd_pub.publish(joint_state_msg);
 		ros::spinOnce();
